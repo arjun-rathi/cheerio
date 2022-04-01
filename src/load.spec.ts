2c50cea5271c192f0b7ae54872b63fffc1de8649
Arjun Rathi <85490710+arjun-rathi@users.noreply.github.com>
2022-04-01 20:26:09 +0530

    Update load.spec.ts

--- src/load.spec.ts
+++ src/load.spec.ts
@@ -7,7 +7,6 @@ describe('.load', () => {
     $('<script>');
     expect($('body')).toHaveLength(1);
   });
- 
   
   it('(html) : should handle lowercase tag options', () => {
     const $ = cheerio.load('<BODY><ul id="fruits"></ul></BODY>', {
